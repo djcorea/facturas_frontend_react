7d6c32da40b78fbf57e251fef417ad6e35e4b643
Dany Corea <danycorea@rhinorealtyinc.com>
2022-04-05 04:17:37 -0600

    products to local stotage

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -1,65 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-export const CardProduct = ({ passChildData, passProduct }) => {
+export const CardProduct = ({ passChildData, passProduct, cart, setCart }) => {
   const [quantity, setQuantity] = useState(1);
-  let [cart, setCart] = useState([])
-  
-  
-  
-  const addItem = (item)  =>   {
-  
-    //create a copy of our cart state, avoid overwritting existing state
-    let cartCopy =  JSON.parse(localStorage.getItem("shoppingCart"));
-    if (cartCopy) {
-    
-    //assuming we have an id field in our item
-    let {id} = item;
-    
-    //look for item in cart array
-    let existingItem = cartCopy.find(cartItem => cartItem.id == id);
-    
-    //if item already exists
-    
-    
-    // cartCopy.push(item)
-    
-      console.log("exists");
-      existingItem.stock += item.stock //update item
-      console.log(existingItem);
-    } else { //if item doesn't exist, simply add it
-      cartCopy.push(item)
-    }
-    
-    //update app state
-    console.log(cartCopy);
-    setCart(cartCopy)
-    
-    console.log(cart);
-    //make cart a string and store in local space
-    let stringCart = JSON.stringify(cartCopy);
-    localStorage.setItem("cart", stringCart)
-    
-  }
-  const updateItem = (itemID, amount) => {}
-  const removeItem = (itemID) => {}
-  
-  //this is called on component mount
-  // useEffect(() => {
-  //   //turn it into js
-  //   console.log("here");
-  //   localCart = JSON.parse(localCart);
-  //   //load persisted cart into state if it exists
-  //   if (localCart) setCart(localCart)
-  //   console.log(cart,"-------");
-    
-  // }, localCart) //the empty array ensures useEffect only runs once
-
-
-
-  // h
-  // e
-  // l
-  // l
+  // const [localCart, setLocalCart] = useState([]);
 
   const handleQuantity = (value) => {
     if (value === -1) {
@@ -72,27 +15,41 @@ export const CardProduct = ({ passChildData, passProduct }) => {
   };
 
   function addCart(product) {
-    // const cartQuantity = localStorage.getItem("cartQuantity");
-    // setCartArray(cartArray.push(product))
-    //   console.log(cartArray);
-    // console.log(localStorage.getItem("shoppingCart"));
-    // // if (cartQuantity>0) {
-    //   // let shoppingCart =JSON.parse(localStorage.getItem("shoppingCart"));
-    //   // setCartArray(cartArray.push(shoppingCart))
-    //   // console.log(JSON.stringify(cartArray));
-    //   // localStorage.setItem("shoppingCart", JSON.stringify(cartArray));
-      
-    // // }else{
-    // //   setCartArray(cartArray.push(product))
-    // //   console.log(cartArray);
-    // //   localStorage.setItem("shoppingCart", JSON.stringify(product));
-      
-    // // }
-    // localStorage.setItem("cartQuantity", parseInt(cartQuantity) + quantity);
-    // passChildData(parseInt(cartQuantity) + quantity);
-    // console.log(cartArray);
+    // let existingItem = localCart.find((productItem)=>productItem.id===product.id);
+    // if (existingItem) {
+    //   existingItem.stock++;
+    // }else{
+    // }
+    const cartQuantity = localStorage.getItem("cartQuantity");
+    let newCart = [...cart];
+    let itemInCart = newCart.find((item) => product.name === item.name);
+    if (itemInCart) {
+      const lessQuantity = itemInCart.quantity;
+      console.log(lessQuantity);
+      localStorage.setItem(
+        "cartQuantity",
+        parseInt(cartQuantity) + quantity - lessQuantity
+      );
+      itemInCart.quantity = +quantity;
+      passChildData(parseInt(cartQuantity) + quantity - lessQuantity);
+    } else {
+      itemInCart = {
+        ...product,
+        quantity: quantity,
+      };
+      newCart.push(itemInCart);
+      localStorage.setItem("cartQuantity", parseInt(cartQuantity) + quantity);
+      passChildData(parseInt(cartQuantity) + quantity);
+    }
+    setCart(newCart);
+
+    // passCart(localCart)
   }
 
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
   return (
     <>
       <div className="col-12 col-md-6  col-lg-4 my-1">
@@ -105,6 +62,9 @@ export const CardProduct = ({ passChildData, passProduct }) => {
           />
           <div className="card-body">
             <h5 className="card-title text-center">{passProduct.name}</h5>
+            <h5 className="card-title text-center text-success">
+              {formatter.format(passProduct.price)}
+            </h5>
             <p className="card-text text-muted">{passProduct.description}</p>
           </div>
           <div className="card-footer">
@@ -143,7 +103,9 @@ export const CardProduct = ({ passChildData, passProduct }) => {
             <div className="row justify-content-around mt-3 mb-3 align-self-end">
               <button
                 className="btb btn-primary col-8 col-md-12 col-lg-8  rounded-pill cursor-btn align-self-center"
-                onClick={()=>{addItem(passProduct)}}
+                onClick={() => {
+                  addCart(passProduct);
+                }}
               >
                 <img
                   src="assets/images/cart.png"
--- src/components/products/ProductsPage.jsx
+++ src/components/products/ProductsPage.jsx
@@ -8,6 +8,15 @@ export const ProductsPage = () => {
   const cartQuantityStorage = localStorage.getItem("cartQuantity");
   const [childData, setChildData] = useState(cartQuantityStorage);
 
+  const localCart = JSON.parse(localStorage.getItem("cart") || "[]");
+  const [cart, setCart] = useState(localCart);
+
+  useEffect(() => {
+    console.log("Hello-dany");
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
   useEffect(() => {
     async function fetchProducts() {
       const { data } = await baseUrl.get("/api/products/");
@@ -17,17 +26,36 @@ export const ProductsPage = () => {
     fetchProducts();
   }, []);
 
-  const handleCartClick = ()=>{
-    if (childData) {
-      console.log("Hello");
+  const handleCartClick = () => {
+    if (childData > 0) {
+      const products = JSON.parse(localStorage.getItem('cart'));
+      console.log(products);
+      const { data } = baseUrl
+        .post("/api/invoice/", {
+          customer_id: 1,
+          products: products,
+        })
+        .then(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      console.log(data, "-----'");
+
+      console.log(childData);
     }
-  }
+  };
 
   const renderedProducts = products.map((product, index) => (
     <CardProduct
       key={product.name}
       passProduct={products[index]}
       passChildData={setChildData}
+      cart={cart}
+      setCart={setCart}
     />
   ));
 
@@ -47,8 +75,9 @@ export const ProductsPage = () => {
       </a> */}
       <hr className="bg-dark border-1 border-top border-dark"></hr>
       <div className="row justify-content-around align-self-center">
-        <button className="btn btn-info col-10 col-md-4 rounded-pill align-self-center"
-        onClick={handleCartClick}
+        <button
+          className="btn btn-info col-10 col-md-4 rounded-pill align-self-center"
+          onClick={handleCartClick}
         >
           <h1 className="mx-2 mt-1 text-light">
             {" "}
@@ -59,15 +88,14 @@ export const ProductsPage = () => {
               width="50"
               height="40"
             />
-              <span className="cartNumber text-light text-lg">
-           <strong className="align-self-center">{childData}</strong>
+            <span className="cartNumber text-light text-lg">
+              <strong className="align-self-center">{childData}</strong>
             </span>
           </h1>
           {/* */}
         </button>
       </div>
-            
-      
+
       <div className="row justify-content-around card-group">
         {renderedProducts}
       </div>
